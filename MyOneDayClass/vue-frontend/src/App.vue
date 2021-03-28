ee9ceb39e0dc35e4f4d635e6860ad59d37d71902
ginaaaaaa <gywlss0212@gmail.com>
2021-03-28 19:58:38 +0900

    main header

--- MyOneDayClass/vue-frontend/src/App.vue
+++ MyOneDayClass/vue-frontend/src/App.vue
@@ -1,81 +1,11 @@
 <template>
   <div>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-     <md-toolbar class="md-primary">
-      <strong><h5 class="md-title">My Oneday Class</h5></strong>
-      <div class="md-layout md-gutter">
-        <div class="md-layout-item"><md-icon class="fa fa-user"></md-icon></div>
-        <div class="md-layout-item"><md-icon class="fa fa-search"></md-icon></div>
-      </div>
-    </md-toolbar>
-          <!-- <div class="md-layout md-gutter">
-        <div class="md-layout-item"><md-icon class="md-size-2x fa fa-home"></md-icon></div>
-        <div class="md-layout-item"><md-icon class="md-size-2x fa fa-sign-in"></md-icon></div>
-      </div> -->
-      <md-button class="md-raised"> Default</md-button>
-      <md-button class="md-raised md-accent">Accent</md-button>
-
-       <div>
-    <md-card>
-      <md-card-header>
-        <div class="md-title">Card without hover effect</div>
-      </md-card-header>
-
-      <md-card-content>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
-      </md-card-content>
-
-      <md-card-actions>
-        <md-button>Action</md-button>
-        <md-button>Action</md-button>
-      </md-card-actions>
-    </md-card>
-
-    <md-card md-with-hover>
-      <md-ripple>
-        <md-card-header>
-          <div class="md-title">Card with hover effect</div>
-          <div class="md-subhead">It also have a ripple</div>
-        </md-card-header>
-
-        <md-card-content>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
-        </md-card-content>
-
-        <md-card-actions>
-          <md-button>Action</md-button>
-          <md-button>Action</md-button>
-        </md-card-actions>
-      </md-ripple>
-    </md-card>
-    {{param}}
-    <button v-on:click="btnClick">Click Me!</button>
-    </div>      
+    <header>
+      <Header></Header>
+    </header>
   </div>
 </template>
 
-<style lang="scss" scoped>
-@import "~vue-material/dist/theme/engine"; // Import the theme engine
-@include md-register-theme("default", (
-  primary:  #09eca8, // The primary color of your brand
-  accent: #32e066  // The secondary color of your brand
-));   // default css impor
-@import "~vue-material/dist/theme/all";
-  .md-title{
-    margin-right : 1200px;
-    font-weight: bold;
-  }
-  .md-layout-item{
-    width: 10px;
-  }
-
-  .md-card {
-  width: 320px;
-  margin: 4px;
-  display: inline-block;
-  vertical-align: top;
-  }
-</style>
 
 <script>
 
@@ -84,10 +14,14 @@ Vue.config.productionTip = false
 import Vue from 'vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
+import Header from './views/commonComponent/Header.vue'
 
 Vue.use(VueMaterial)
 
 export default {
+  components : {
+    'Header' : Header
+  },
   data: function(){
     return{
       param : "param start"
---
+++ MyOneDayClass/vue-frontend/src/views/commonComponent/Header.vue
@@ -0,0 +1,78 @@
+<template>
+<div>
+  <div class="gnb">
+    <div class="wrapper">
+      <div style="font-weight: 600; font-size: x-large;">MyOnedayClass</div> 
+      <div style="margin-left:800px;"><md-button class="md-raised md-primary">강사 로그인</md-button></div>
+      <div><md-button class="md-raised md-primary">회원가입</md-button></div>
+    </div>                               
+  </div>
+  <md-toolbar md-transparent>
+    <md-button style="width:130px;">자수/손뜨개</md-button>
+    <md-button style="width:130px;">요리/베이킹</md-button>
+    <md-button>공예</md-button>
+    <md-button>운동</md-button>
+    <md-button style="width:130px;">캘리/드로잉</md-button>
+    <md-button style="width:130px;">비누/캔들</md-button>                 
+  </md-toolbar>
+</div>    
+</template>
+
+<style>
+.gnb{
+  display : flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 1520px;
+  height: 60px;
+}
+.wrapper{
+  display : flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 1200px;
+  height: 60px;
+}
+</style>
+
+<style lang="scss" scoped>
+@import "~vue-material/dist/theme/engine"; // Import the theme engine
+@include md-register-theme("default", (
+  primary:  #b302ca5e, // The primary color of your brand
+  accent: #32e066  // The secondary color of your brand
+));   // default css impor
+@import "~vue-material/dist/theme/all";
+
+
+.md-layout-item{
+  width: 130px;
+}
+.md-toolbar{
+  padding-left: 130px;
+}
+</style>
+<script>
+Vue.config.productionTip = false
+
+import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+
+
+Vue.use(VueMaterial)
+
+export default {
+  data: function(){
+    return{
+    }
+  },    
+  methods:  {
+  btnClick(){
+    alert('12');
+  }
+    }
+  }
+</script>
+
