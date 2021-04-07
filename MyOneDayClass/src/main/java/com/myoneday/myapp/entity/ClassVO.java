8e9bc70f5452c07c87dd235707a9b4124e737d47
ginaaaaaa <gywlss0212@gmail.com>
2021-04-07 21:26:32 +0900

    lombok 의존성 추가, 적용

--- MyOneDayClass/src/main/java/com/myoneday/myapp/entity/ClassVO.java
+++ MyOneDayClass/src/main/java/com/myoneday/myapp/entity/ClassVO.java
@@ -1,35 +1,13 @@
 package com.myoneday.myapp.entity;
 
+import lombok.Data;
+
+@Data
 public class ClassVO {
 
 	private String className;
 	private String classDescribe;
 	private String classLike;
 	private String categoryName;
-	
-	public String getClassName() {
-		return className;
-	}
-	public void setClassName(String className) {
-		this.className = className;
-	}
-	public String getClassDescribe() {
-		return classDescribe;
-	}
-	public void setClassDescribe(String classDescribe) {
-		this.classDescribe = classDescribe;
-	}
-	public String getClassLike() {
-		return classLike;
-	}
-	public void setClassLike(String classLike) {
-		this.classLike = classLike;
-	}
-	public String getCategoryName() {
-		return categoryName;
-	}
-	public void setCategoryName(String categoryName) {
-		this.categoryName = categoryName;
-	}
 	
 }
